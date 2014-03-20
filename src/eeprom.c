f1d46f41cc94ecc5008e00a77c8dea30ba6653cc
Alan Barr <alanbarr2002@hotmail.com>
2014-03-20 16:56:04 +0000

    eeprom reading

--- src/eeprom.c
+++ src/eeprom.c
@@ -26,19 +26,13 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
-
-#define HOST 0 /* 1 for linux */
-
-#if HOST != 1
 #include "fyp.h"
-#else
-#include <stdio.h>
-#define PRINT(...) printf(__VA_ARGS__)
-#endif
 
-#define EEPROM_BASE     0x08080000
-#define EEPROM_LAST     0x08081FFF
-#define EEPROM_SIZE     (EEPROM_LAST - EEPROM_BASE + 1)
+#define EEPROM_BASE         0x08080000
+#define EEPROM_LAST         0x08081FFF
+#define EEPROM_SIZE         (EEPROM_LAST - EEPROM_BASE + 1)
+
+#define STORE_ADDR_EEPROM   ((uint32_t *)EEPROM_BASE)
 
 #define compile_time_assert(X)                                              \
     extern int (*compile_assert(void))[sizeof(struct {                      \
@@ -56,16 +50,71 @@ typedef struct {
 
 compile_time_assert(sizeof(eepromData) == 8); /* We don't want any padding */
 
-#if HOST != 1
-static eepromStore * const pStore = (eepromStore *)EEPROM_BASE;
-static eepromData * const  pData = &(((eepromStore*)EEPROM_BASE)->data);
-#else 
-static eepromStore store;
-static eepromStore * const pStore = &store;
-static eepromData * const pData = &(store.data);
-#endif
+/* As per: 4.1.1 Unlocking the Data EEPROM block and the FLASH_PECR register in
+ * PM0062 Programming Manual */
+static int32_t eepromUnlock(void)
+{
+#define PEKEY1 0x89ABCDEF
+#define PEKEY2 0x02030405
+    FLASH->PEKEYR = PEKEY1;
+    FLASH->PEKEYR = PEKEY2;
 
-uint32_t generateChecksum(const void * data, uint32_t bytes)
+    /* Should be cleared - 0 = success */
+    return (FLASH->PECR & FLASH_PECR_PELOCK);
+}
+
+/* As per: 4.1.1 Unlocking the Data EEPROM block and the FLASH_PECR register in
+ * PM0062 Programming Manual */
+static int32_t eepromLock(void)
+{
+    FLASH->PECR |= FLASH_PECR_PELOCK;
+    return 0;
+}
+
+/* As per: 4.3.6 Data EEPROM Word Write in PM0062 Programming Manual */
+static int32_t eepromWriteWords(uint32_t * address, const uint32_t * data,  uint32_t size)
+{
+    chSysLock();
+    size /= sizeof(uint32_t);
+
+    if (eepromUnlock())
+    {
+        return 1;
+    }
+
+    FLASH->PECR |= FLASH_PECR_FTDW; 
+
+    while (size > 0)
+    {
+        *address = *data;
+        address++;
+        data++;
+        size--;
+    }
+
+    eepromLock();
+
+    chSysUnlock();
+
+    return 0;
+}
+
+static int32_t eepromReadWords(const uint32_t * address, uint32_t * data, uint32_t size)
+{
+    size /= sizeof(uint32_t);
+
+    while (size > 0)
+    {
+        *data = *address;
+        address++;
+        data++;
+        size--;
+    }
+
+    return 0;
+}
+
+static uint32_t generateChecksum(const void * data, uint32_t bytes)
 {
     uint32_t checksum = 0;
     uint8_t index = 0;
@@ -84,52 +133,68 @@ uint32_t generateChecksum(const void * data, uint32_t bytes)
     return checksum;
 }
 
-bool checksumUpdate(void)
+static int32_t checksumUpdate(eepromStore * store)
 {
-    pStore->checksum = generateChecksum(pData, sizeof(*pData));
-    return true;
-}
-
-bool checksumOk(void)
-{
-    uint32_t generatedChecksum = generateChecksum(pData, sizeof(*pData));
-
-    if (generatedChecksum == pStore->checksum)
-    {
-        return true;
-    }
-    return false;
-}
-
-
-#if HOST == 1
-int main(void)
-{
-    memset(pStore, 0, sizeof(*pStore));
-    pStore->data.lastShutdownError = 0x55;
-    pStore->data.unresponsiveShutdowns = 0x53;
-    PRINT("generated checksum: %x\n", generateChecksum(pData, sizeof(*pData)));
-    checksumUpdate();
-    PRINT("generated ok?: %d\n", checksumOk());
-
+    store->checksum = generateChecksum(&(store->data), sizeof(store->data));
     return 0;
 }
 
-#else
-
-bool chibios_test_eeprom(void)
+static bool checksumOk(eepromStore * store)
 {
-    memset(pStore, 0, sizeof(*pStore));
-    pStore->data.lastShutdownError = 0x55;
-    pStore->data.unresponsiveShutdowns = 0x53;
-    PRINT("generated checksum: %x\n", generateChecksum(pData, sizeof(*pData)));
-    checksumUpdate();
-    PRINT("generated ok?: %d\n", checksumOk());
+    if (generateChecksum(store, sizeof(*store)) == 0)
+    {
+        return true;
+    }
 
-    pStore->data.unresponsiveShutdowns = 0x50;
-    PRINT("generated ok?: %d\n", checksumOk());
     return false;
 }
 
+
+#if 0
+bool chibios_test_eeprom(void)
+{
+    eepromStore tempStore;
+    eepromStore tempStoreRead;
+    uint32_t chksum;
+    bool b;
+
+    memset(&tempStore,0,sizeof(tempStore));
+    memset(&tempStoreRead,0,sizeof(tempStore));
+
+    tempStore.data.lastShutdownError = 0x55;
+    tempStore.data.unresponsiveShutdowns = 0x53;
+
+    chksum = generateChecksum(&tempStore, sizeof(tempStore));
+
+    PRINT("generated checksum: %x\n", chksum);
+    checksumUpdate(&tempStore);
+    b = checksumOk(&tempStore);
+    PRINT("generated ok?: %d\n", b);
+
+    /* Write stucture to eeprom */
+    eepromWriteWords(STORE_ADDR_EEPROM, (uint32_t*)&tempStore, sizeof(tempStore));
+
+    /* Read Structure from eeprom */
+    eepromReadWords(STORE_ADDR_EEPROM, (uint32_t*)&tempStoreRead, sizeof(tempStoreRead));
+ 
+    b = checksumOk(&tempStoreRead);
+    PRINT("Read ok?: %d\n", b);
+
+
+    if (memcmp(&tempStore, &tempStoreRead, sizeof(tempStore)) == 0)
+    {
+        PRINT("memcmp ok", NULL);
+    }
+    else
+    {
+        PRINT("memcmp NOT ok", NULL);
+    }
+
+    b = checksumOk(&tempStoreRead);
+
+    PRINT("generated ok?: %d\n", b);
+
+    return false;
+}
 #endif
 
